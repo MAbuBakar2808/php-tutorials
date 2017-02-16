c810f674a68c68876f256a3cabb7b6ecf1b1031f
MAbuBakar <muhammadabubakar256@gmail.com>
2017-02-17 01:39:12 +0500

    Switch in php

---
+++ switch.php
@@ -0,0 +1,27 @@
+<?php
+
+$counter = 1;
+
+switch ($counter) {
+	case '1':
+		echo "Number one found";
+		break;
+	case '2':
+		echo "Number two found";
+		break;
+	case '3':
+		echo "Number three found";
+		break;
+	
+	default:
+		echo "Number not found";
+		break;
+}
+
+
+
+
+
+
+
+?>
