4f6394ff614e22246cb2fa8272fc8672f384223b
MAbuBakar <muhammadabubakar256@gmail.com>
2017-02-17 05:36:44 +0500

    How to convert string case upper lower in php

---
+++ string-shuffle-function.php
@@ -0,0 +1,13 @@
+<?php
+
+$string = "This is the basic String function";
+
+$string_shuffled = str_shuffle($string);
+
+echo $string_shuffled;
+
+
+
+
+
+?>
---
+++ stringlower-upper.php
@@ -0,0 +1,16 @@
+<?php
+
+$string = 'M.AbuBakar';
+
+//$string_lower = strtolower($string);
+
+//echo $string_lower;
+
+
+$string_upper = strtoupper($string);
+
+echo $string_upper;
+
+
+
+?>
